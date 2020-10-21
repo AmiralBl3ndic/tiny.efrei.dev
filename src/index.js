8287b65dfd1516b6e2b0361a766097f81e7c4b4a
AmiralBl3ndic <camille.briand@efrei.net>
2020-10-21 10:05:18 +0200

    feat: implement most of the features

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
+const { promisify } = require('util');
 const dotenv = require('dotenv');
+const redis = require('redis');
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const { StatusCodes } = require('http-status-codes');
+const { nanoid } = require('nanoid');
+
+const urlRegistrationYupModel = require('./model/url-register-request.yup');
 
 dotenv.config();
+
+if (!process.env.REDIS_HOST) {
+  throw new Error('Missing REDIS_HOST environment variable');
+}
+
+const redisClient = redis.createClient(`redis://${process.env.REDIS_HOST}`);
+
+/**
+ * Async wrapper for Redis client
+ */
+const store = {
+  get: promisify(redisClient.get).bind(redisClient),
+  set: promisify(redisClient.set).bind(redisClient),
+};
 
 const app = express();
 
@@ -15,9 +35,70 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
-    code: 200,
-    message: 'This works',
+    message: 'A simple URL reducer',
   });
+});
+
+app.post('/url', async (req, res) => {
+  try {
+    urlRegistrationYupModel.validate(req.body);
+  } catch (error) {
+    return res.status(StatusCodes.CONFLICT).json({
+      error,
+    });
+  }
+
+  /**
+   * URL to register
+   */
+  const urlToRegister = req.body.url;
+  const slug = nanoid(10);
+  await store.set(slug, urlToRegister);
+
+  res.status(StatusCodes.CREATED).json({
+    url: urlToRegister,
+    slug,
+    _links: {
+      _self: {
+        method: 'POST',
+        url: '/url',
+      },
+      registerURL: {
+        method: 'POST',
+        url: '/url',
+      },
+      redirect: {
+        path: '/url/:slug',
+      },
+    },
+  });
+});
+
+app.all('/url/:slug', async (req, res) => {
+  const url = await store.get(req.params.slug);
+
+  if (!url) {
+    return res.status(StatusCodes.NOT_FOUND).json({
+      error: true,
+      message: `Found no registered URL for slug ${req.params.slug}`,
+      _links: {
+        _self: {
+          method: req.method,
+          url: req.url,
+          path: req.path,
+        },
+        registerURL: {
+          method: 'POST',
+          url: '/url',
+        },
+        redirect: {
+          path: '/url/:slug',
+        },
+      },
+    });
+  }
+
+  res.redirect(url);
 });
 
 app.listen(process.env.PORT || 8080, () => {
